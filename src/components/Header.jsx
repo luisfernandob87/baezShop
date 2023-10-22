08f8553327db61021cd44ac115f19bd491ef94d2
luis.morales <luisfernandob87@gmail.com>
2023-10-21 18:02:24 -0600

    add features 21/10

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import "../styles/header.css";
-import cart from "../assets/cart_white.svg";
-import cuenta from "../assets/user_white.svg";
+import logo from "../assets/logo.png";
 import search from "../assets/search.svg";
 function Header() {
   return (
     <header>
       <div className="containerHeader">
-        <div>LOGO</div>
+        <div>
+          <img src={logo} alt="Logo" />
+        </div>
         <div className="containerInput">
           <div className="buscador">
             <img src={search} alt="Icono buscar" />
@@ -15,11 +16,34 @@ function Header() {
         </div>
         <div className="containerIconos">
           <div className="icono">
-            <img src={cuenta} alt="Icono Cuenta" />
+            <svg
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
             <span>Cuenta</span>
           </div>
           <div className="icono">
-            <img src={cart} alt="Icono Carrito" />
+            <svg
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+              />
+            </svg>
+
             <span>Carrito</span>
           </div>
         </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,126 @@
+import "../styles/nav.css";
 function Nav() {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <ul className="containerNav">
+        <li className="listaCategorias">
+          <svg
+            className="iconosCategorias"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M17 8v-3a1 1 0 0 0 -1 -1h-10a2 2 0 0 0 0 4h12a1 1 0 0 1 1 1v3m0 4v3a1 1 0 0 1 -1 1h-12a2 2 0 0 1 -2 -2v-12" />
+            <path d="M20 12v4h-4a2 2 0 0 1 0 -4h4" />
+          </svg>
+          Articulos de cuero
+        </li>
+        <li className="listaCategorias">
+          <svg
+            className="iconosCategorias"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M6 5a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2v-14z" />
+            <path d="M11 4h2" />
+            <path d="M12 17v.01" />
+          </svg>
+          Celulares y accesorios
+        </li>
+        <li className="listaCategorias">
+          <svg
+            className="iconosCategorias"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 13m0 2a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2z" />
+            <path d="M15 13m0 2a2 2 0 0 1 2 -2h1a2 2 0 0 1 2 2v3a2 2 0 0 1 -2 2h-1a2 2 0 0 1 -2 -2z" />
+            <path d="M4 15v-3a8 8 0 0 1 16 0v3" />
+          </svg>
+          Audifonos
+        </li>
+        <li className="listaCategorias">
+          <svg
+            className="iconosCategorias"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M3 19l18 0" />
+            <path d="M5 6m0 1a1 1 0 0 1 1 -1h12a1 1 0 0 1 1 1v8a1 1 0 0 1 -1 1h-12a1 1 0 0 1 -1 -1z" />
+          </svg>
+          Cómputo y tablets
+        </li>
+        <li className="listaCategorias">
+          <svg
+            className="iconosCategorias"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M9.785 6l8.215 8.215l-2.054 2.054a5.81 5.81 0 1 1 -8.215 -8.215l2.054 -2.054z" />
+            <path d="M4 20l3.5 -3.5" />
+            <path d="M15 4l-3.5 3.5" />
+            <path d="M20 9l-3.5 3.5" />
+          </svg>
+          Electrónicos
+        </li>
+        <li className="listaCategorias">
+          <svg
+            className="iconosCategorias"
+            width="35"
+            height="35"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#000000"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 6v3" />
+            <path d="M14 6v3" />
+            <path d="M5 9m0 2a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z" />
+            <path d="M12 15m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+            <path d="M9 3h6v3h-6z" />
+          </svg>
+          Perfumería
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Nav;
